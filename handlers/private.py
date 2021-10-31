1d94d8ceed0d9333ed15010c6d4e49fe4b380a8a
Percy <percy@railway.app>
2021-10-31 13:23:39 +0000

    Initial Commit

---
+++ handlers/private.py
@@ -0,0 +1,47 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    
+    await message.reply_text(
+        f"""**
+🌠𝗧𝗵𝗶𝘀 𝗜𝘀 𝗔𝗱𝘃𝗮𝗻𝗰𝗲 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 \n🌺𝗥𝘂𝗻 𝗢𝗻 𝗣𝗿𝗶𝘃𝗮𝘁𝗲 𝗩𝗣𝗦 𝗦𝗲𝗿𝘃𝗲𝗿 \n🌼𝗙𝗲𝗲𝗹 𝗛𝗶𝗴𝗵 𝗤𝘂𝗮𝗹𝗶𝘁𝘆 𝗠𝘂𝘀𝗶𝗰 𝗜𝗻 𝗩𝗖 \n⭐𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗱 𝗕𝘆 [Natttuu](https://t.me/natttuu)**
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "❰𝗢𝘄𝗻𝗲𝗿❱", url="https://t.me/natttuu")
+                  ],[
+                    InlineKeyboardButton(
+                        "❰𝗦𝘂𝗽𝗽𝗼𝗿𝘁❱", url="https://t.me/Natking_support"
+                    ),
+                    InlineKeyboardButton(
+                        "❰𝗚𝗿𝗼𝘂𝗽❱", url="https://t.me/friendsforever_i"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "❰Commands❱", url="https://telegra.ph/NatKing-Music-Bot-Command-10-19"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("alive") & ~filters.private & ~filters.channel & ~filters.group)
+async def gstart(_, message: Message):
+      await message.reply_text("""**𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 𝗢𝗻𝗹𝗶𝗻𝗲 𝗡𝗼𝘄\n🌠NatKing <3**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌼𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url="https://t.me/friendsforever_i")
+                ]
+            ]
+        )
+   )
